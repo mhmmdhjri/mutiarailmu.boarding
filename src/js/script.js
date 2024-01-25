23d8f6d3f681f3d567b5510c2e14d97354bf54e2
mhmmdhjri <muhammadhijri25@gmail.com>
2024-01-25 07:44:32 +0700

    second commit

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,34 @@ hamburger.addEventListener('click',function(){
  navMenu.classList.toggle('hidden');
   
 })
+
+// dm
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click',function(){
+    if(darkToggle.checked){
+        html.classList.add('dark');
+    }else{
+        html.classList.remove('dark');
+    }
+});
+darkToggle.addEventListener('click',function(){
+    if(darkToggle.checked){
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    }else{
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+})
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+    darkToggle.checked = true
+  } else {
+    document.documentElement.classList.remove('dark')
+    darkToggle.checked = false
+  }
+
+
